7360fed69e565fed86830e9b655a9192b70920dd
Michał Stanik <micha.stanik@gmail.com>
2019-08-03 14:38:15 +0200

    SeedWroldHeritage

--- MCB/MCB.Data/MCBDictionarySeeder.cs
+++ MCB/MCB.Data/MCBDictionarySeeder.cs
@@ -1,4 +1,6 @@
 ﻿using MCB.Data.Domain.Geo;
+using MCB.Data.Domain.WorldHeritages;
+using MCB.Data.LoadData;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using static MCB.Data.LoadData.CountriesModel;
 
 namespace MCB.Data
@@ -27,7 +30,65 @@ namespace MCB.Data
             _context.Database.Migrate();
 
             await SeedCountries();
+            await SeedWroldHeritage();
         }
+
+        private async Task SeedWroldHeritage()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Rows));
+            TextReader textReader = new StreamReader(@"C:/Users/micha/source/repos/MCB/MCB/MCB.Data/LoadData/WorldHeritage.xml");
+            Rows worldHeritage;
+
+            worldHeritage = (Rows)deserializer.Deserialize(textReader);
+
+            var worldHeritageList = new List<WorldHeritage>();
+            var listOfLinkedEntities = new List<WorldHeritageCountry>();
+
+            foreach (var item in worldHeritage.Row)
+            {
+                var listOfCountries = item.Iso_code.ToUpper().Split(',').ToList();
+
+                var newWorldHeritage = new WorldHeritage()
+                {
+                    UnescoId = item.Id_number,
+                    ImageUrl = item.Image_url,
+                    IsoCodes = item.Iso_code,
+                    Latitude = item.Latitude,
+                    Longitude = item.Longitude,
+                    Location = item.Location,
+                    Region = item.Region
+                };
+
+                var listOfCountriesFromDb = new List<Country>();
+
+                foreach (var country in listOfCountries)
+                {
+                    var countryFromDb = _context.Country.Where(c => c.Alpha2Code == country).FirstOrDefault();
+
+                    if (countryFromDb != null)
+                    {
+                        listOfCountriesFromDb.Add(countryFromDb);
+
+                        if (listOfCountries.Count() == listOfCountriesFromDb.Count())
+                        {
+                            foreach (var countryToIclude in listOfCountriesFromDb)
+                            {
+                                listOfLinkedEntities.Add(new WorldHeritageCountry { Country = countryToIclude, WorldHeritage = newWorldHeritage });
+                            }
+                        }
+                    }
+                }
+
+                worldHeritageList.Add(newWorldHeritage);
+            }
+
+            _context.AddRange(worldHeritageList);
+            _context.AddRange(listOfLinkedEntities);
+            await _context.SaveChangesAsync();
+
+            textReader.Close();
+        }
+
 
         private async Task SeedCountries()
         {
